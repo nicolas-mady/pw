8d1fe24eb9536e8d88a432346b11319e5f3dcf84
Nicolas Mady <nicolasmady06@gmail.com>
2026-04-29 08:51:16 -0400

    .

--- mod2/node/node1/1.1.js
+++ mod2/node/node1/1.1.js
@@ -1,14 +1,20 @@
 const http = require('http');
-const fs = require('fs/promises');
+const fs = require('fs');
 const path = require('path');
 
 const PORT = 5678;
 
-async function main() {
+function main() {
 
-    const dir_name = process.argv[2];
-    const dir_content = await fs.readdir(dir_name);
-    // console.log(dir_content);
+    // let dirs = [];
+
+    // process.argv.slice(2).forEach(dir => {
+    //     dirs.push(dir);
+    // });
+
+    // const content = dirs.map(_ => fs.readdirSync(_));
+    // console.log(content);
+    
 
     // function readDirectory(dir) {
     //     let content;
@@ -29,7 +35,25 @@ async function main() {
 
     const server = http.createServer((req, res) => {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end(dir_content.join('\n'));
+
+        fs.readdir(process.argv[2], (err, files) => {
+            if (err) {
+                res.end('Error reading directory');
+                return;
+            }
+            let content = '';
+            files.forEach(file => {
+                if (fs.statSync(path.join(process.argv[2], file)).isDirectory()) {
+                    // content += `"dir": ${file}\n`;
+                    return;
+                }
+                const filePath = path.join(process.argv[2], file);
+                const fileContent = fs.readFileSync(filePath, 'utf-8');
+                content += `"file": ${file}, "content": ${fileContent}\n`;
+            });
+            res.end(content);
+            // res.end(files.join('\n'));
+        });
     });
 
     server.listen(PORT, () => {
